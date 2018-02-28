60c5a2cfc50cfde7323833b9d17ebfed2f16da87
a07208trng_b4a.01.10 <a07208trng_b4a.01.10@BDC4-D-5RB1RG2.dir.svc.accenture.com>
2018-02-28 11:53:06 +0530

    content change

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -21,9 +21,9 @@ public class HelloTest {
 	public void beforeTest() {
 		/*	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
 		driver = new FirefoxDriver();*/
-		System.setProperty("webdriver.chrome.driver","C:\javarepo\SampleJavaProject\resources\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("http://localhost:8080/Helloworld.war/");
+		driver.get("http://localhost:8082/HelloWorld.war/");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 	}
 
